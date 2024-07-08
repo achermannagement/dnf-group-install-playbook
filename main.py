4c8d1fc37beed245d41504b6c8071adeb4337b68
Joshua Achermann <joshua.achermann@gmail.com>
2024-07-08 16:05:48 +1000

    Fix main to output file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+import pathlib
 import subprocess
 
 from dnf_group_info_parser import DnfGroupInfoParser
@@ -7,6 +8,7 @@ from ansible_playbook_emitter import AnsiblePlaybookEmitter
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("group_name", type=str, help="name of group to install")
+    parser.add_argument("output_folder", type=pathlib.Path, help="output directory to ")
     return parser.parse_args()
 
 def main(args):
@@ -15,6 +17,12 @@ def main(args):
     dnf_output = process.stdout.decode().split('\n')
     for dnf_line in list(filter(None, dnf_output)):
         dnf_parser.parse(dnf_line)
+    abe = AnsiblePlaybookEmitter(dnf_parser)
+    # filename will just be based on group name
+    output_folder = args.output_folder if args.output_folder.is_dir() else args.output_folder.parent
+    path = output_folder.joinpath(f"{dnf_parser.group}_dnf_group.yaml")
+    fh = open(path, 'w')
+    fh.write(abe.emit())
 
 if __name__ == "__main__":
     args = parse_args()
