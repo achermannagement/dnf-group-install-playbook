503481b0115e31459a0d376d035fed95d2f2cb7d
Joshua Achermann <joshua.achermann@gmail.com>
2024-07-08 16:05:35 +1000

    Finalise ansible playbook parser

--- ansible_playbook_emitter.py
+++ ansible_playbook_emitter.py
@@ -2,31 +2,14 @@
 Simple emitter for dnf group info into ansible playbook
 
 No I/O, just string is emitted
-
-FIXME: use yaml instead of raw text
 """
+import yaml
+
 class AnsiblePlaybookEmitter:
 
-    INDENT_SIZE = 2
-    INDENT = " " * INDENT_SIZE
+    DEFAULT_PREAMBLE = {'hosts':'localhost', 'become':'yes', 'become_method': 'sudo'}
 
-    DEFAULT_PREAMBLE = """
-- hosts: localhost
-  become: yes
-  become_method: sudo
-  tasks:
-"""
-
-    DEFAULT_POSTAMBLE = """
-
-"""
-
-    INSTALL_TASK = """
-- name: install {pkg_name}
-  package:
-    name: {pkg_name}
-    state: present
-"""
+    DEFAULT_POSTAMBLE = {} #FIXME
 
     def __init__(self, dnf_parser, preamble=None, start_indent=None):
         self._preamble = preamble if preamble else self.DEFAULT_PREAMBLE
@@ -34,13 +17,16 @@ class AnsiblePlaybookEmitter:
         self._dnf_parser = dnf_parser
 
     def emit(self):
-        results = ""
-        results += self._preamble
-        results += "\n"
+        results = []
+        results.append(self.DEFAULT_PREAMBLE)
+        results[0]['tasks'] = []
+
         # using list comprehension for flattening the dict_value lists
         all_pkgs = [x for xs in self._dnf_parser.pkg_lists.values() for x in xs]
+
         for pkg_name in all_pkgs:
-            results += self.INDENT * self._indent
-            results += self.INSTALL_TASK.format(pkg_name=pkg_name)
-        results += self.DEFAULT_POSTAMBLE
-        return results
+            results[0]['tasks'].append(
+                    {'name': f'install {pkg_name}',
+                     'package': {'name': pkg_name, 'state': 'present'}})
+
+        return yaml.dump(results)
